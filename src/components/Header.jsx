b951a2928428a5d36b4aca70e693fb346d529059
Dialtor <dialtordev@gmail.com>
2022-09-09 22:58:07 -0500

    first commit vite + tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='text-4xl font-bold uppercase text-center mt-20'>Welcome to Vite + Tailwind</div>
+  )
+}
+
+export default Header
